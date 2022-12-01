16d2cbb90a11733142eede4ea498f3685a3276e3
Sophia Lee <sophia.anna.lee@gmail.com>
2022-11-30 23:09:35 -0500

    Improved accuracy

--- setup.py
+++ setup.py
@@ -10,27 +10,26 @@ def install_requirements(GPU_integration: bool = False) -> None:
         "python data/TACO/download_script.py" # downloads TACO images (NOTE: FCPS wifi results in a connection error) and generates YOLO v5 annotations
     ])
 
-    if GPU_integration: # NOTE: for GPU-PyTorch integration (optional), install CUDA-11.7.0 and cuDNN-8.6.0 (https://youtu.be/hHWkvEcDBO0?t=205)
+    if GPU_integration: # NOTE: for GPU-PyTorch integration (optional), install CUDA-11.7.0 and cuDNN-8.6.0
         run_in_terminal([ 
             "pip uninstall torch", # remove torch-1.13.0+cpu (default installation is CPU only)
             "pip3 install torch==1.13.0+cu117 torchvision==0.14.0+cu117 torchaudio===0.13.0+cu117 -f https://download.pytorch.org/whl/cu117/torch_stable.html",
         ])
 
-install_requirements()
+# install_requirements()
 
 print(f"PyTorch: {torch.__version__}\t TorchVision: {torchvision.__version__}\t TorchAudio: {torchaudio.__version__}\n" +\
       f"CUDA enabled: {torch.cuda.is_available()}\t CuDNN enabled: {torch.backends.cudnn.enabled}\n")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def compile_model(pretrained: bool = False) -> None:
-    if pretrained:
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-        # TODO: finish this
-    else:
-        run_in_terminal([
-            "python train.py --img 640 --batch 16 --epochs 10 --data TACO.yaml --weights yolov5s.pt --name trash_classifier --cache disk",
-            "python detect.py --weights runs/train/trash_classifier/weights/best.pt --img 640 --conf 0.4 --source data/TACO/images/test"
-        ])
+def train_and_run(epochs: int, data: str, cfg: str, weights: str, source: str, name: str, img: int, conf: float) -> None:
+    run_in_terminal([
+        f"python train.py --epochs {epochs} --img {img} --data {data} --cfg {cfg} --weights {weights} --name {name} --cache disk",
+        f"python detect.py --img {img} --weights runs/train/{name}/weights/best.pt --conf {conf} --source {source} --name {name}"
+    ])
 
-compile_model()
+# train_and_run(3, "data/coco128.yaml", "", "weights/yolov5s.pt", "data/coco128/images/train2017", "coco128_classifier", 640, 0.25)
+
+# run_in_terminal(["python detect.py --weights weights/yolov5s.pt --source data/TACO/images/"]) 
+train_and_run(100, "data/TACO.yaml", "models/yolov5s_nc60.yaml", "weights/yolov5s.pt", "data/TACO/images/test", "trash_classifier", 640, 0.1)
