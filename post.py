ac052aca64e26d299584f14b53f9e2db4212d7ac
Sophia Lee <sophia.anna.lee@gmail.com>
2022-12-16 13:19:21 -0500

    Removed another import

--- post.py
+++ post.py
@@ -1,4 +1,4 @@
-import torch, torchvision, torchaudio, pandas, numpy as np
+import torch, torchvision, torchaudio, pandas
 
 print(f"PyTorch: {torch.__version__}\t TorchVision: {torchvision.__version__}\t TorchAudio: {torchaudio.__version__}\n" +\
       f"CUDA enabled: {torch.cuda.is_available()}\t CuDNN enabled: {torch.backends.cudnn.enabled}\n")
